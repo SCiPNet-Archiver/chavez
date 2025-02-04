27b987c4d4459d546e127a5cdfc8bf68d0a3fac4
Raynard Miot <rayhmiot@gmail.com>
2025-02-04 18:54:26 -0500

    Initialized Spring project

---
+++ src/main/java/com/cantomiletea/chavez/ChavezApplication.java
@@ -0,0 +1,13 @@
+package com.cantomiletea.chavez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChavezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChavezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cantomiletea/chavez/ChavezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cantomiletea.chavez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChavezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
